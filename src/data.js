164428d4bb8fc83f0e70793e6caeb1e8ef8a35fe
martingp1991 <92342847+martingp1991@users.noreply.github.com>
2023-11-10 11:56:47 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const imageData = [
   {
     title: "A hero section in dark mode",
-    imageUrl: "https://v0.dev/api/fotaoxa/image/edge",
+    imageUrl: "https://uideck.com/wp-content/uploads/2023/02/startup-nextjs-1024x519.png",
     avatarUrl: "https://vercel.com/api/www/avatar/mXLS0Jh2eWmJ2thq3UHgoTVF?s=64",
     description: "5 days ago",
       variant1ImageUrl: "https://v0.dev/_next/image?url=%2Fshare%2FZXXB7Tv%2Fimage&w=3840&q=75",
@@ -10,7 +10,7 @@ const imageData = [
   },
   {
     title: "A Tweet UI",
-    imageUrl: "https://v0.dev/api/AfXYpLG/image/edge",
+    imageUrl: "https://www.piecex.com/product_image/20220416072747-00004127-reactjslandingpagetemplate-cropped.png",
     avatarUrl: "https://vercel.com/api/www/avatar/llAlLUCrmQJRYkqXtfPvfWOd?s=64",
     description: "5 days ago",
       variant1ImageUrl: "https://v0.dev/_next/image?url=%2Fshare%2FZXXB7Tv%2Fimage&w=3840&q=75",
@@ -19,7 +19,7 @@ const imageData = [
   },
   {
     title: "A sleek pricing page for a SaaS.",
-    imageUrl: "https://v0.dev/api/bo2N7pU/image/edge",
+    imageUrl: "https://freebiesbug.com/wp-content/uploads/2020/04/open-react-template.png",
     avatarUrl: "https://vercel.com/api/www/avatar/llAlLUCrmQJRYkqXtfPvfWOd?s=64",
     description: "5 days ago",
       variant1ImageUrl: "https://v0.dev/_next/image?url=%2Fshare%2FZXXB7Tv%2Fimage&w=3840&q=75",
